782e5154644e1163cb9f2b4549bbc50dec33be22
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-11-17 14:13:26 +0000

    Deploy AliceO2Group/QualityControl to github.com/AliceO2Group/QualityControl.git:gh-pages

---
+++ navtreedata.js
@@ -0,0 +1,52 @@
+var NAVTREE =
+[
+  [ "QualityControl", "index.html", [
+    [ "Advanced topics", "md_doc_Advanced.html", null ],
+    [ "Repository backends benchmark", "md_doc_benchmark-repo.html", null ],
+    [ "Developers' tips", "md_doc_DevelopersTips.html", null ],
+    [ "Frequently asked questions", "md_doc_FAQ.html", null ],
+    [ "Modules development", "md_doc_ModulesDevelopment.html", null ],
+    [ "Post-processing", "md_doc_PostProcessing.html", null ],
+    [ "QuickStart", "md_doc_QuickStart.html", null ],
+    [ "New features", "md_doc_ReleaseNotesTemplate.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Deprecated List", "deprecated.html", null ],
+    [ "Namespaces", null, [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+".html",
+"InfrastructureGenerator_8cxx.html",
+"Triggers_8cxx.html#a8fbad73b3fa2a1fb29892f804fea8c3e",
+"classo2_1_1quality__control_1_1core_1_1QualityObject.html#a5b8075ffd843b2a221252071712c1148",
+"classo2_1_1quality__control__modules_1_1common_1_1NonEmpty.html#a5a7f2bd19719b145198aa47a5dd9bf4b",
+"classo2_1_1quality__control__modules_1_1mft_1_1BasicClusterQcCheck.html#a1d5522fe3a4d79197024e54dbe7ae526",
+"classo2_1_1quality__control__modules_1_1muonchambers_1_1TH1MCHReductor.html#a5b186521065bbe553c5044217d660a88",
+"classo2_1_1quality__control__modules_1_1tof_1_1PostProcessDiagnosticPerCrate.html",
+"runDataDump_8cxx.html#a9c6d7f8d628391c1bad26be1f1abc271",
+"testCcdbDatabase_8cxx.html#a6b2a3852db8bb19ab6909bac01859985"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
